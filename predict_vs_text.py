2bc7f9494a6b24bdd3a98f78c83e5b9d5dfb1307
Victor Morozov <7209059@gmail.com>
2022-08-08 09:29:26 +0300

    fixed

--- predict_vs_text.py
+++ predict_vs_text.py
@@ -7,9 +7,9 @@ Created on Sun Aug  7 21:06:43 2022
 from catboost import CatBoostClassifier, Pool
 import pandas as pd
 
-train_df = pd.read_csv('DB/rest.csv', index_col = 0, sep = ';', encoding='utf-8-sig', on_bad_lines='skip')
+X_rest = pd.read_csv('DB/rest.csv', index_col = 0, sep = ';', encoding='utf-8-sig', on_bad_lines='skip')
 
-X_rest = train_df.drop(['date', 'user'], axis = 1)
+X_rest = X_rest.drop(['date', 'user'], axis = 1)
 X_rest = X_rest.fillna('other')
 
 cat_features = ['city', 'moscow_district', 'region', 'area']
@@ -20,6 +20,6 @@ model = CatBoostClassifier()
 
 prediction = model.load_model('model/cat_model_vs_text', format='cbm').predict_proba(X_rest)
 
-train_df['predict'] = pd.Series(prediction[:, 1])
+X_rest['predict'] = pd.Series(prediction[:, 1])
 
-train_df.to_csv('DB/prediction_vs_test.csv', sep = ';', encoding='utf-8-sig')
+X_rest.to_csv('DB/prediction_vs_test.csv', sep = ';', encoding='utf-8-sig')
